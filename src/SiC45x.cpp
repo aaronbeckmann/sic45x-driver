b29ce314f6e85c2b3f63b8a341aba207d3fa270f
Aaron Beckmann <aaron.beckmann26@gmail.com>
2024-04-29 16:29:23 +0200

    test

--- src/SiC45x.cpp
+++ src/SiC45x.cpp
@@ -12,7 +12,7 @@ SiC45x::SiC45x(uint8_t i2cAddress) : SiC45x(i2cAddress, Wire) {}
 SiC45x::SiC45x(uint8_t i2cAddress, TwoWire& wire) : smbus_(i2cAddress, wire) {}
 
 bool SiC45x::begin(uint8_t i2cAddress, TwoWire& wire){
-  smbus_(i2cAddress, wire);
+  smbus_(&i2cAddress, wire);
   return smbus_.begin();
 }
 bool SiC45x::begin() { return smbus_.begin(); }
